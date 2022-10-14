6d4177c89d26f82ea32200687de2473f124aaca7
caroline Bauer <bauercarol@hotmail.com>
2022-10-14 11:18:34 -0300

    promises e describe

---
+++ curso-cypress/cypress/integration/describe.spec.js
@@ -0,0 +1,19 @@
+it('testes externo', () => {
+    
+});
+
+describe('should group tests...',() =>{
+    describe('Should group more specific tests...',() =>{
+        it.skip('A specific test...', () => {
+    
+        });
+    })
+    describe('Should group more specific tests...',() =>{
+        it('A specific test 2...', () => {
+    
+        });
+    })
+    it('testes externo', () => {
+    
+    });
+})
