c18eed83ff9531c82bf9e51fe1f2fd8909f0233e
carolbauer <bauercarol@hotmail.com>
2023-01-24 14:21:07 -0300

    renomeando variável

--- Bootcamp-GFT-QA/java-anatomia-classes/src/MinhaClasse1.java
+++ Bootcamp-GFT-QA/java-anatomia-classes/src/MinhaClasse1.java
@@ -1,19 +1,18 @@
 import javax.print.DocFlavor.STRING;
 
-public class MinhaClasse1{
- 
-    public static void main(String[]args){
-     String primeiroNome = "Caroline";
-     String segundoNome = "Bauer";
-     
-     String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+public class MinhaClasse1 {
 
-     System.out.println(nomeCompleto);
+    public static void main(String[] args) {
+        String primeiroNome = "Caroline";
+        String segundoNome = "Bauer";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
 
-    public static String nomeCompleto (String primeiroNome, String segundoNome){
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return "Resultado do método" + " " + primeiroNome.concat(" ").concat(segundoNome);
     }
-
 
 }
