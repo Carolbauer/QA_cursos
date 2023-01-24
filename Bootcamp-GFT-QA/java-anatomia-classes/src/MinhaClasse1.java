4ee3a670f6cb0ed5bcfe1418c8689e6e12f40bcd
carolbauer <bauercarol@hotmail.com>
2023-01-24 13:43:30 -0300

    Declarando variáveis e métodos

--- Bootcamp-GFT-QA/java-anatomia-classes/src/MinhaClasse1.java
+++ Bootcamp-GFT-QA/java-anatomia-classes/src/MinhaClasse1.java
@@ -1,7 +1,19 @@
-public class MinhaClasse1 {
-    public static void man (String [] args){
-        final String BR = "Brasil";
-        
-        BR = "Brazil";
+import javax.print.DocFlavor.STRING;
+
+public class MinhaClasse{
+ 
+    public static void main(String[]args){
+     String primeiroNome = "Caroline";
+     String segundoNome = "Bauer";
+     
+     String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+     System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método" + " " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+
 }
