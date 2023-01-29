9cbbbe7f6143d84955c7b47f171b02872a378b68
carolbauer <bauercarol@hotmail.com>
2023-01-29 16:27:02 -0300

    Desafio e Junit

---
+++ Bootcamp-GFT-QA/Desafios-Codigo/src/QuantidadeNecessaria.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class QuantidadeNecessaria {
+    public static void main(String[] args) {
+        final int SLICE = 4;
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        // TODO: Retorne o número de pizzas necessárias para atender o pedido.
+
+        for (int i =0; i <T ++){
+            int N =
+        }
+
+
+
+    }
+}
