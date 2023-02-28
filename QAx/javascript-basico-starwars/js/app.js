8a8ca611c8d78bfc38908d40ca6bd8075e0dc99d
carolbauer <caroline.bauer@ubistart.com>
2023-02-27 21:06:59 -0300

    variaveis js

--- QAx/javascript-basico-starwars/js/app.js
+++ QAx/javascript-basico-starwars/js/app.js
@@ -1,1 +1,16 @@
 console.log('olá Javascript!')
+
+var userName = 'Caroline Bauer'
+
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
+
+
+
+
